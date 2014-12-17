6f4d247526d51fe4d3dd37e81d09afc4c35b2c2f
Ha Anh Man <anhmantk@gmail.com>
2014-12-17 08:14:23 +0700

    commit code

--- moreapp.php
+++ moreapp.php
@@ -65,6 +65,7 @@ $app = mysql_fetch_assoc($result);
         <meta charset="UTF-8">               
         <link href="/css/moreapp.css" rel="stylesheet" />
         <script src="/js/jquery.js" type="text/javascript"></script>
+        <meta name="viewport" content="width=device-width">
     </head>
     <body>
         <div id="container">
